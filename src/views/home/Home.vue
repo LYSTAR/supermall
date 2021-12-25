1a863c3e8d9878576875096da9491a62e6935ffc
LYSTAR <liyuxing1997@foxmail.com>
2021-12-25 12:59:26 +0800

    default

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,50 +1,62 @@
 <template>
-    <div id="home" class="wrapper">
-        <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    </div>
+  <div id="home" class="wrapper">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <swiper>
+      <swiper-item
+        v-for="(item, index) in banners"
+        v-bind:item="item"
+        v-bind:index="index"
+        :key="index"
+      >
+        <a :href="item.link">
+          <img :src="item.image" alt="" />
+        </a>
+      </swiper-item>
+    </swiper>
+  </div>
 </template>
 
 <script>
-
-import NavBar from 'components/common/navbar/NavBar'
+import NavBar from "components/common/navbar/NavBar";
+import { getHomeMultidata, getHomeGoods } from "network/home";
+import { Swiper, SwiperItem } from "components/common/swiper";
 
 export default {
-    name:"Home",
-    components: {
-       NavBar
+  name: "Home",
+  components: {
+    NavBar,
+    Swiper,
+    SwiperItem,
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+    };
+  },
+  computed: {},
+  destroyed() {},
+  activated() {},
+  deactivated() {},
+  created() {
+    this.getHomeMutidata();
+  },
+  mounted() {},
+  methods: {
+    getHomeMutidata() {
+      getHomeMultidata().then((res) => {
+        console.log(res.data.banner.list);
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
     },
-    data () {
-        return {
-            
-        }
-    },
-    computed: {
-        
-    },
-    destroyed () {
-        
-    },
-    activated () {
-        
-    },
-    deactivated () {
-        
-    },
-    created () {
-        
-    },
-    mounted () {
-        
-    },
-    methods: {
-        
-    }
-}
+  },
+};
 </script>
 
 <style scoped>
 .home-nav {
-    background-color: var(--color-tint);
-    color: #fff;
+  background-color: var(--color-tint);
+  color: #fff;
 }
 </style>
